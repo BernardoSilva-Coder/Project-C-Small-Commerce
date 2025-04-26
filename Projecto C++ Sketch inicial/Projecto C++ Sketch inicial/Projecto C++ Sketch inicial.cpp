843b68b20856edc97829a88be16fae81f94f6d7f
Bernardo Silva <judasmanuel2018@gmail.com>
2025-04-26 08:40:33 +0100

    Update Projecto C++ Sketch inicial.cpp
    Efectuar Venda sem talao ainda e algumas validacoes  , actualiza cao da string que regista os detalhes de venda para ostringstream

--- Projecto C++ Sketch inicial/Projecto C++ Sketch inicial/Projecto C++ Sketch inicial.cpp
+++ Projecto C++ Sketch inicial/Projecto C++ Sketch inicial/Projecto C++ Sketch inicial.cpp
@@ -3,8 +3,10 @@
 
 #include <iostream>
 #include <iomanip>
-#include <string>
-#include <ctime>
+#include <string> // Premite usar o to_string
+#include <ctime> //TODO: Acrescentar data e hora ao talao
+#include <locale> // Usar caracteres como o c cedilhado e assentos e o simbolo do euro
+#include <sstream> //Para Puder Usar ostringstream
 
 using namespace std;
 
@@ -12,27 +14,32 @@ using namespace std;
 int numMaxProd = 50;
 int colunas = 4;
 int totalProdutos = 4;
-double IVA = 1.3;
+const double IVA = 1.23;
+const double margemLucro = 1.30;
 // Matriz Para stock de 50 items diferentes ( ID: 0 | Nome: 1 | Quantidade : 2 | Preco de custo : 3)
 string** stock = new string * [numMaxProd];
 
-void inicializarstock(string** stock) {
+void inicializarstock() {
     for (int i = 0; i < numMaxProd; i++)
     {
         stock[i] = new string[colunas];
     }
+    //Primeiro Produto
     stock[0][0] = "1";
     stock[0][1] = "Pao";
     stock[0][2] = "100";
     stock[0][3] = "0.04";
+    //Segundo Produto
     stock[1][0] = "2";
     stock[1][1] = "Arroz";
     stock[1][2] = "150";
     stock[1][3] = "1.00";
+    //Terceiro Produto
     stock[2][0] = "3";
     stock[2][1] = "Atum";
     stock[2][2] = "100";
     stock[2][3] = "0.50";
+    //Quarto Produto
     stock[3][0] = "4";
     stock[3][1] = "Milho";
     stock[3][2] = "100";
@@ -50,7 +57,7 @@ void listarProdutos(string** stock) {
     {
         for (int j = 0; j < colunas; j++)
         {
-            cout << stock[i][j] << "\t";
+            cout << stock[i][j] << fixed << setprecision(2) << "\t";
         }
 
         cout << endl;
@@ -59,7 +66,7 @@ void listarProdutos(string** stock) {
 //Criacao da Funcao Adicionar Produto
 void adicionarProdutos(string** stock) {
     // Variáveis
-    int numeroProdAdd;
+    int numeroProdAdd, idProd = 1;
 
 
     // Quantos produtos vai adicionar?
@@ -96,7 +103,7 @@ void removerProdutos(string** stock) {
     cout << "Qual o ID do Produto que quer apagar? ";
     cin >> idProdApg;
 
-    int index = 0;                             // A Variável index serve para guardar a posição (linha) onde está o produto que tem o ID que o utilizador quer remover.
+    int index = -1;                             // A Variável index serve para guardar a posição (linha) onde está o produto que tem o ID que o utilizador quer remover.
     for (int i = 0; i < totalProdutos; i++) {
         if (stoi(stock[i][0]) == idProdApg) {   //stoi(stock[i][0]) significa converter a variavel da matriz ID (Coluna 0 que e uma string para Int, de modo a poder ser comparada)
             index = i;                          // Dai aqui a igualarmos a posicao, variavel que nos da a posicao.
@@ -104,7 +111,7 @@ void removerProdutos(string** stock) {
         }
     }
 
-    if (index <= 0) {
+    if (index == -1) {
         cout << "ID não encontrado." << endl;
         return;
     }
@@ -126,29 +133,99 @@ void removerProdutos(string** stock) {
     totalProdutos--; // Atualiza o total de Produtos
     cout << "Produto removido com sucesso." << endl;
 }
+//Criacao da Funcao Efectuar venda
 void efectuarVenda(string** stock) {
-    
+    int idProd;
+    if (totalProdutos == 0) //Validacao do Stock
+    {
+        cout << "Nao Existe Produtos em Stock ";
+        return;
+    }
 
+    char continuar;
+    double totalsemIva = 0;
+    //Criacao de String que vai guardar os detahes da compra
+    string detalhesVenda = "";
 
+    do {
+        listarProdutos(stock);
 
+        // Procurar produto pelo ID
+        int index = -1;
+        cout << "Insira o ID do Produto que Pretende Adicionar a Compra : ";
+        cin >> idProd;
+        for (int i = 0; i < totalProdutos; i++) {
+            if (stoi(stock[i][0]) == idProd) {
+                index = i;
+                break;
+            }
+        }
 
+        if (index == -1) {
+            cout << endl << "ID " << idProd << " não encontrado." << endl;
+            return;
+        }
+        //Variaveis Para fazer os calculos dos custos
+        int quantidadeStock = stoi(stock[index][2]);
+        int qntdVenda;
+        cout << "Insira a Quantidade do Produto que deseja comprar : ";
+        cin >> qntdVenda;
 
+        if (qntdVenda <= 0) {
+            cout << endl << "A quantidade introduzida não é válida." << endl;
+            return;
+        }
+        if (qntdVenda > quantidadeStock) {
+            cout << endl << "Nao existe quantidade suficiente em stock desse produto." << endl;
+            return;
+        }
 
+        // Calculo do preço
+        double precoUnitario = stod(stock[index][3]) * margemLucro;
+        double subtotal = precoUnitario * qntdVenda;
+        totalsemIva += subtotal;
 
+        // Atualizar stock
+        stock[index][2] = to_string(quantidadeStock - qntdVenda); //linha funciona como um cout invisível. // Formatei tudo como queria que saisse a string no talao  // Para evitar que to_string() ponha casas decimais a mais.
 
+        // Adicionar à string do talão atraves do "OSTRINGSTREAM" : Para formatar números (com setprecision, fixed, etc.) antes de transformar em string.Para juntar vários valores num só texto, de forma organizada.
+        ostringstream linha;
+        linha << left << setw(15) << stock[index][1] << setw(15) << qntdVenda << setw(15) << precoUnitario << setw(15) << precoUnitario * qntdVenda << "\n";
+        detalhesVenda += linha.str();// Puxei o resultado para o detalhesVenda com linha.str().
 
+        cout << qntdVenda << " unidade do produto " << stock[index][1] << " foi adicionado ao carrinho. Subtotal: " << fixed << setprecision(2) << subtotal << " €" << endl;
+        cout << "Deseja continuar a comprar? (s/n): ";
+        cin >> continuar;
 
+    } while (continuar == 's' || continuar == 'S');
 
+    double totalComIva = totalsemIva * IVA;
+    cout << detalhesVenda;
+
+    /* // TODO:Imprimir talão com o ostringstreeam para o formatar direitinho (Nao estou  a conseguir inprinmir)
+    ostringstream totais;
+    totais << fixed << setprecision(2);
+    totais << "\n====== TALÃO DE COMPRA ======\n";
+    totais << left << setw(15) << "Produto" << setw(15) << "Qtd" << setw(15) << "Preço Unitário" << setw(15) << "Subtotal" << "\n";
+    totais << "----------------------------------------------------------------------------------------------------------------\n";
+    totais << detalhesVenda;
+    totais << "----------------------------------------------------------------------------------------------------------------\n";
+    totais << left << setw(30) << "Total sem IVA:" << totalsemIva << " €\n";
+    totais << left << setw(30) << "Total com IVA:" << totalComIva << " €\n";
+    totais << "=============================\n";
+    // left	Alinha o texto à esquerda.
+    // setw(15)	Reserva 15 caracteres de largura para o camp
+    //fixed << setprecision(2)	Força sempre 2 casas decimais.
+    cout << totais.str();*/ 
 }
-
 int main()
 {
+    setlocale(LC_ALL, ".65001");
     inicializarstock();
     int op;
     do {
-
-        system("CLS");// Limpa o menu para a proxima operação
-
+        system("CLS");// Limpa o menu para a proxima operação             TODO:Validacoes Para que quando escrevemos uma letras no menu nao de crash
+        cout << endl;
         cout << "--- MENU ---" << endl;
         cout << "1. Efetuar venda" << endl;
         cout << "2. Adicionar produto" << endl;
@@ -157,23 +234,18 @@ int main()
         cout << "5. Sair" << endl;
         cout << "Opcao: ";
         cin >> op;
-    
 
         switch (op) {
-        case 1:; break; // criar funcao para efectuarVenda + Talao
+        case 1: efectuarVenda(stock); break; // criar funcao para efectuarVenda + Talao
         case 2: adicionarProdutos(stock); break; // criar funcao para adicionarProdutos
         case 3: removerProdutos(stock); break; // criar funcao de eliminarProdutos
         case 4: listarProdutos(stock); break; // criar a funcao de listarPprodutos, tendo em base o stock
         case 5: cout << "A sair..." << endl; break;
         default: cout << "Opção inválida." << endl;
         }
+        cout << "\nPressione Enter para continuar...";
+        cin.ignore(); // Limpa o buffer do cin
+        cin.get(); // Espera o Enter
 
-        if (op != 5) {
-            cout << "\nPressione Enter para continuar...";
-            cin.ignore(); // Limpa o buffer do cin
-            cin.get(); // Espera o Enter
-        }
-        
     } while (op != 5);
-
-
+}
